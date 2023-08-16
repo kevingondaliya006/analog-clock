8d5a585559037bf2a3bfa6397df78fca530d84c5
kevingondaliya006 <kevinpatel00612@gmail.com>
2023-08-17 03:17:13 +0930

    all done sound left

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-function show_clock()
-{
+
+setInterval(show_clock, 1000);
+
+function show_clock() {
     let h = document.getElementsByClassName('hr')[0];
     let m = document.getElementsByClassName('mn')[0];
     let s = document.getElementsByClassName('ss')[0];
 
-    let date = new date();
+    let date = new Date();
 
-    let hours =date.getHours();
+    let hours = date.getHours();
     let minutes = date.getMinutes();
-    let seconds =  date.getSeconds();
-
+    let seconds = date.getSeconds();
     h.style.transform = `rotate(${30 * hours + minutes / 2}deg)`;
     m.style.transform = `rotate(${6 * minutes}deg)`;
     s.style.transform = `rotate(${6 * seconds}deg)`;
 
-    let sound = new Audio('sound.mp3')
+    let sound = document.getElementById('sound.mp3');
     sound.play();
 
 }
 
-setInterval(show_clock, 1000);
+let intervalId = setInterval(show_clock, 1000);
+
+// Clear the interval when the page is unloaded
+window.addEventListener('unload', () => {
+    clearInterval(intervalId);
+});
